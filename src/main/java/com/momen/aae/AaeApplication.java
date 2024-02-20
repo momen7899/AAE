f3612012375778ab1c434778210463707ee33dec
momen.hamaweisi <momen.hamaweisi@digikala.com>
2024-02-20 17:16:40 +0330

    init project

---
+++ src/main/java/com/momen/aae/AaeApplication.java
@@ -0,0 +1,13 @@
+package com.momen.aae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momen/aae/AaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momen.aae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
