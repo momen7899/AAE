6805dc326091858f66b06735f6d07fadd9e64be4
momen.hamaweisi <momen.hamaweisi@digikala.com>
2024-02-20 17:26:04 +0330

    init products

---
+++ products/src/test/java/com/momen/aee/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momen.aee.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
